320ee0e12101d4d81f6aeb9aeb8e40cc632d799c
Grácouš <84779291+LordGracous@users.noreply.github.com>
2023-07-11 16:47:53 +0200

    Delete Uzivatel.js

--- Uzivatel.js
+++
@@ -1,9 +0,0 @@
-class Uzivatel {
-
-    constructor(firstName, lastName, age, phoneNumber) {
-        this.name = `${firstName} ${lastName}`
-        this.age = age,
-        this.phoneNumber = phoneNumber
-    }
-    
-}
